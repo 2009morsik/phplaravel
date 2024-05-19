ac29a9679afbca66abe69bcae2cbadb17c71030d
2009morsik <2009morsik@mail.ru>
2024-05-20 00:08:16 +0500

    new branch lb2

--- blog-master/routes/web.php
+++ blog-master/routes/web.php
@@ -15,3 +15,11 @@ Route::get('/info/server', [UserController::class, 'phpmyinfo'])->name('server')
 Route::get('/info/client', [UserController::class, 'clientip']);
 
 Route::get('/info/database', [UserController::class, 'dtb']);
+
+Route::post('api/auth/login', 'App\Http\Controllers\Auth\RegisterController@login');
+Route::post('api/auth/register', 'App\Http\Controllers\Auth\RegisterController@create');
+Route::post('api/auth/out', 'App\Http\Controllers\Auth\RegisterController@out');
+Route::post('api/auth/out_all', 'App\Http\Controllers\Auth\RegisterController@out_all');
+
+
+Route::post('/register', [RegisterController::class, 'store']);
